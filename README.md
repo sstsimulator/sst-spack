5dee8930e0355c6f892841e6d9da33bddc1b5753
jjwilke <jjwilke@sandia.gov>
2020-03-12 08:21:20 -0700

    Added initial core package

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# sst-spack
+![SST](http://sst-simulator.org/img/sst-logo-small.png)
+
+# Structural Simulation Toolkit (SST) Spack Packages
+
+#### Copyright (c) 2009-2018, Sandia National Laboratories
+Sandia National Laboratories is a multimission laboratory managed and operated
+by National Technology and Engineering Solutions of Sandia, LLC., a wholly 
+owned subsidiary of Honeywell International, Inc., for the U.S. Department of 
+Energy's National Nuclear Security Administration under contract DE-NA0003525.
+
+## 
+Make sure you have downloaded [Spack](https://github.com/spack/spack) and added it to your path.
+The easiest way to do this is often (depending on your SHELL):
+````
+source spack/share/spack/setup-env.sh 
+````
+
+To get the most up-to-date version of the SST Spack packages, after downloading the sst-spack GitHub repository, you simply need to run
+````
+spack repo add sst-spack/sst
+````
+To validate that Spack now sees the repo with the SST packages, run:
+````
+spack repo list
+````
+This should now list your newly downloaded Spack repo.
+You can display information about how to install the individual packages with, e.g.:
+````
+spack info sst-core
+````
+This will print all the information about variants and dependencies of the package.
+For detailed instructions on how to use Spack, see the [Owner's Manual](https://spack.readthedocs.io).
---
+++ sst/packages/sst-core/package.py
@@ -0,0 +1,52 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class SstCore(AutotoolsPackage):
+    """Kokkos implements a programming model in C++ for writing performance
+    portable applications targeting all major HPC platforms."""
+
+    homepage = "https://github.com/sstsimulator"
+    git      = "https://github.com/sstsimulator/sst-core.git"
+
+    version('9.1',     url="https://github.com/sstsimulator/sst-core/releases/download/v9.1.0_Final/sstcore-9.1.0.tar.gz",
+            sha256="cfeda39bb2ce9f32032480427517df62e852c0b3713797255e3b838075f3614d")
+    version('devel',   branch='devel')
+    version('master',  branch='master', preferred=True)
+
+    variant("pdes",   default=True, description="Build support for parallel discrete event simulation")
+    variant("zoltan", default=False, description="Use Zoltan for partitioning parallel runs")
+    variant("hdf5",   default=False, description="Build support for HDF5 statistic output")
+    variant("zlib",   default=False, description="Build support for ZLIB compression")
+
+    depends_on("python")
+    depends_on("mpi", when="+pdes")
+    depends_on("zoltan", when="+zoltan")
+    
+    @when('@devel')
+    @when('@master')
+    def autoreconf(self, spec, prefix):
+        bash = which('bash')
+        bash('autogen.sh')
+
+    def configure_args(self):
+      args = []
+      if "+zoltan" in self.spec:
+        args.append("--with-zoltan=%s" % self.spec["zoltan"].prefix)
+      if "+hdf5" in self.spec:
+        args.append("--with-hdf5=%s" % self.spec["hdf5"].prefix)
+      if "+zlib" in self.spec:
+        args.append("--with-zlib=%s" % self.spec["zlib"].prefix)
+
+      if "+pdes" in self.spec:
+        args.append("--enable-mpi")
+      else:
+        args.append("--disable-mpi")
+
+      args.append("--with-python=%s" % self.spec["python"].prefix)
+
+      return args
