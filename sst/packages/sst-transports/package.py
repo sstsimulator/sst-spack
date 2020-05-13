113b27a00b373c6cb68fc31880bc7693af04a5dd
jjwilke <jjwilke@sandia.gov>
2020-05-12 21:49:07 -0700

    added sst-transports repo

---
+++ sst/packages/sst-transports/package.py
@@ -0,0 +1,23 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class SstTransports(CMakePackage):
+    """Provides transports like uGNI and verbs
+       that run in the simulator"""
+
+    homepage = "https://github.com/sstsimulator"
+    git      = "https://github.com/jjwilke/sst-transports.git"
+
+    version('master',  branch='master', preferred=True)
+
+    depends_on("sst-macro")
+
+    def cmake_args(self):
+      args = []
+      args.append("-DSSTMacro_ROOT=%s" % self.spec["sst-macro"].prefix)
+      return args
